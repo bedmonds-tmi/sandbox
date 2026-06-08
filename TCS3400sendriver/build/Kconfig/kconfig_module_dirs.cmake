c86459663dd5113daa95a3b32dc1d7c607c52f09
derrell <derrell.nofiel@trudellmed.com>
2026-06-08 14:40:04 -0400

    Added TCS3400 driver

---
+++ TCS3400sendriver/build/Kconfig/kconfig_module_dirs.cmake
@@ -0,0 +1,3 @@
+set(kconfig_env_dirs)
+list(APPEND kconfig_env_dirs ZEPHYR_CMSIS_6_MODULE_DIR=C:/Users/derrell.nofiel/dev/projects/test/deps/modules/hal/cmsis_6)
+list(APPEND kconfig_env_dirs ZEPHYR_HAL_NORDIC_MODULE_DIR=C:/Users/derrell.nofiel/dev/projects/test/deps/modules/hal/nordic)
