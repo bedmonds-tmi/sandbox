376cd1a0bf2f8aea028d4c879b6c0af0461aba77
Fayez Saad <fayez.saad@trudellmed.com>
2026-06-30 16:05:04 -0400

    drivers: add xgzp6897d and bme280

---
+++ lib/api/pressure/inc/tmi/api/pressure.h
@@ -0,0 +1,85 @@
+#ifndef TMI_API_PRESSURE_H
+#define TMI_API_PRESSURE_H
+
+#include <stdint.h>
+#include <zephyr/device.h>
+
+typedef struct {
+	uint16_t dig_T1; ///< temperature compensation value
+	int16_t dig_T2;  ///< temperature compensation value
+	int16_t dig_T3;  ///< temperature compensation value
+
+	uint16_t dig_P1; ///< pressure compensation value
+	int16_t dig_P2;  ///< pressure compensation value
+	int16_t dig_P3;  ///< pressure compensation value
+	int16_t dig_P4;  ///< pressure compensation value
+	int16_t dig_P5;  ///< pressure compensation value
+	int16_t dig_P6;  ///< pressure compensation value
+	int16_t dig_P7;  ///< pressure compensation value
+	int16_t dig_P8;  ///< pressure compensation value
+	int16_t dig_P9;  ///< pressure compensation value
+
+	uint8_t dig_H1; ///< humidity compensation value
+	int16_t dig_H2; ///< humidity compensation value
+	uint8_t dig_H3; ///< humidity compensation value
+	int16_t dig_H4; ///< humidity compensation value
+	int16_t dig_H5; ///< humidity compensation value
+	int8_t dig_H6;  ///< humidity compensation value
+} bme280_cal_t;
+
+typedef struct {
+	const struct device *bus;
+	uint8_t addr;
+	uint8_t filter;
+	uint8_t hum;
+	uint8_t temp;
+	uint8_t press;
+
+} tmi_pressure_config_t;
+
+typedef struct {
+	int32_t t_fine;
+	int32_t t_fine_adjust;
+	bme280_cal_t cal;
+} tmi_pressure_data_t;
+
+typedef struct tmi_pressure_s tmi_pressure_t;
+
+typedef struct {
+	int (*init)(tmi_pressure_t *dev);
+	int (*get_whoami)(tmi_pressure_t *dev);
+	int (*get_humidity)(tmi_pressure_t *dev, float *RH);
+	int (*get_pressure)(tmi_pressure_t *dev, float *Pascals);
+	int (*get_tempurature)(tmi_pressure_t *dev, float *degC);
+} tmi_pressure_api_t;
+
+struct tmi_pressure_s {
+	tmi_pressure_config_t config;
+	tmi_pressure_data_t data;
+	const tmi_pressure_api_t *api;
+};
+
+static inline int tmi_pressure_init(tmi_pressure_t *dev)
+{
+	return dev->api->init(dev);
+}
+static inline int tmi_pressure_whoami(tmi_pressure_t *dev)
+{
+	return dev->api->get_whoami(dev);
+}
+static inline int tmi_pressure_get_humidity(tmi_pressure_t *dev, float *rh)
+{
+	return dev->api->get_humidity(dev, rh);
+}
+
+static inline int tmi_pressure_get_pressure(tmi_pressure_t *dev, float *pa)
+{
+	return dev->api->get_pressure(dev, pa);
+}
+
+static inline int tmi_pressure_get_temp_mC(tmi_pressure_t *dev, float *mC)
+{
+	return dev->api->get_tempurature(dev, mC);
+}
+
+#endif /* TMI_API_PRESSURE_H */
---
+++ lib/drivers/bme280/bme280.c
@@ -0,0 +1,389 @@
+
+// #include "bme280.h"
+#include <tmi/api/pressure.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/sys/byteorder.h>
+#include "bme280_defs.h"
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(bme280);
+
+#define BME280_REGISTER_DIG_T1       0x88
+#define BME280_REGISTER_DIG_T2       0x8A
+#define BME280_REGISTER_DIG_T3       0x8C
+#define BME280_REGISTER_WHOAMI       0xD0
+#define BME280_REGISTER_VERSION      0xD1
+#define BME280_REGISTER_SOFTRESET    0xE0
+#define BME280_REGISTER_CAL26        0xE1
+#define BME280_REGISTER_CONTROLHUMID 0xF2
+#define BME280_REGISTER_STATUS       0XF3
+#define BME280_REGISTER_CONTROL      0xF4
+#define BME280_REGISTER_CONFIG       0xF5
+
+#define BME280_REGISTER_PRESSUREDATA_H 0xF7
+#define BME280_REGISTER_TEMPDATA_H     0xFA
+#define BME280_REGISTER_HUMIDDATA_H    0xFD
+
+#define BME280_I2C_ADDR0 0x76
+#define BME280_I2C_ADDR1 0x77
+
+// standard register values
+#define BME280_VALUE_RESET 0xB6
+
+// device, address, where you want to put it, lenght
+static int read_reg(const tmi_pressure_t *dev, uint8_t reg, uint8_t *val, uint8_t len)
+{
+	return i2c_write_read(dev->config.bus, dev->config.addr, &reg, 1, val, len);
+}
+
+static int write_reg(const tmi_pressure_t *dev, uint8_t reg, uint8_t val)
+{
+	return i2c_reg_write_byte(dev->config.bus, dev->config.addr, reg, val);
+}
+
+static int write_mask(tmi_pressure_t *dev, uint8_t reg, uint8_t mask, uint8_t val)
+{
+	uint8_t tmp;
+
+	int ret = read_reg(dev, reg, &tmp, sizeof(tmp));
+	if (ret != 0) {
+		printk("Error: %d", ret);
+		return ret;
+	}
+	tmp &= ~mask;                 // Clear the target bit
+	tmp |= FIELD_PREP(mask, val); // Set the target bit based on val
+	return write_reg(dev, reg, tmp);
+}
+
+/*!
+ *   @brief  Reads the factory-set coefficients
+ */
+static int bme280_readCoefficients(tmi_pressure_t *dev)
+{
+
+	uint8_t val[2];
+
+	read_reg(dev, BME280_REGISTER_DIG_T1, val, sizeof(val));
+	dev->data.cal.dig_T1 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_T2, val, sizeof(val));
+	dev->data.cal.dig_T2 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_T3, val, sizeof(val));
+	dev->data.cal.dig_T3 = sys_get_le16(val);
+
+	read_reg(dev, BME280_REGISTER_DIG_P1, val, sizeof(val));
+	dev->data.cal.dig_P1 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P2, val, sizeof(val));
+	dev->data.cal.dig_P2 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P3, val, sizeof(val));
+	dev->data.cal.dig_P3 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P4, val, sizeof(val));
+	dev->data.cal.dig_P4 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P5, val, sizeof(val));
+	dev->data.cal.dig_P5 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P6, val, sizeof(val));
+	dev->data.cal.dig_P6 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P7, val, sizeof(val));
+	dev->data.cal.dig_P7 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P8, val, sizeof(val));
+	dev->data.cal.dig_P8 = sys_get_le16(val);
+	read_reg(dev, BME280_REGISTER_DIG_P9, val, sizeof(val));
+	dev->data.cal.dig_P9 = sys_get_le16(val);
+
+	uint8_t val1, val2;
+
+	read_reg(dev, BME280_REGISTER_DIG_H1, &val1, sizeof(val1));
+	dev->data.cal.dig_H1 = val1;
+
+	read_reg(dev, BME280_REGISTER_DIG_H2, val, sizeof(val));
+	dev->data.cal.dig_H2 = sys_get_le16(val);
+
+	read_reg(dev, BME280_REGISTER_DIG_H3, &val1, sizeof(val1));
+	dev->data.cal.dig_H3 = val1;
+
+	read_reg(dev, BME280_REGISTER_DIG_H4, &val1, sizeof(val));
+	read_reg(dev, BME280_REGISTER_DIG_H4 + 1, &val2, sizeof(val2));
+	dev->data.cal.dig_H4 = (val1 << 4) | (val2 & 0xF);
+
+	read_reg(dev, BME280_REGISTER_DIG_H5 + 1, &val1, sizeof(val1));
+	read_reg(dev, BME280_REGISTER_DIG_H4, &val2, sizeof(val2));
+
+	dev->data.cal.dig_H5 = ((val1) << 4) | (val2 >> 4);
+
+	read_reg(dev, BME280_REGISTER_DIG_H6, &val1, sizeof(val1));
+	dev->data.cal.dig_H6 = val1;
+
+	return 0;
+}
+
+static int bme280_whoami(tmi_pressure_t *dev)
+{
+	if (dev == NULL) {
+		return -EINVAL;
+	}
+	uint8_t tmp;
+	int ret = read_reg(dev, BME280_REGISTER_WHOAMI, &tmp, sizeof(tmp));
+	if (ret != 0) {
+		return ret;
+	}
+	LOG_INF("WHO AM I Register reads  0x%02X ", tmp);
+	return 0;
+}
+
+static int bme280_reset(tmi_pressure_t *dev)
+{
+	if (dev == NULL) {
+		return -EINVAL;
+	}
+
+	int ret = write_reg(dev, BME280_REGISTER_SOFTRESET, BME280_VALUE_RESET);
+	if (ret != 0) {
+		return ret;
+	}
+	printk("Reset ....");
+
+	return 0;
+}
+
+static int bme280_activate_device(tmi_pressure_t *dev)
+{
+	uint8_t val;
+	int ret = read_reg(dev, BME280_REGISTER_CONTROL, &val, sizeof(val));
+	if (ret != 0) {
+		return ret;
+	}
+	printk("Control register parameters :  0x%02X ", val);
+
+	ret = write_reg(dev, BME280_REGISTER_CONTROL, 0x03);
+	if (ret != 0) {
+		return ret;
+	}
+}
+
+static bme280_scale_general_t bme280_num_to_scale_sel(uint32_t factor)
+{
+
+	if (factor < 2) {
+		return BME280_GENERAL_OS_x2;
+	} else if (factor < 4) {
+		return BME280_GENERAL_OS_x4;
+	} else if (factor < 8) {
+		return BME280_GENERAL_OS_x8;
+	} else if (factor < 16) {
+		return BME280_GENERAL_OS_x16;
+	} else {
+		printk("bme280 can't achieve %d factor, clamping to 16 G", factor);
+		return BME280_GENERAL_OS_x16;
+	}
+}
+
+static int bme280_init_humidity(tmi_pressure_t *dev, uint8_t num)
+{
+	if (num >= BME280_GENERAL_OS_x16 && num < 0) {
+		printk("Invalid gyro fs range.");
+		return -ERANGE;
+	}
+	bme280_scale_general_t humidity_s = bme280_num_to_scale_sel(num);
+
+	int ret = write_reg(dev, BME280_REGISTER_CONTROLHUMID, (uint8_t)humidity_s);
+	if (ret != 0) {
+		return ret;
+	}
+	dev->config.hum = num;
+	uint8_t x;
+	read_reg(dev, BME280_REGISTER_CONTROLHUMID, &x, sizeof(x));
+	printk("reading of control register 0x%02X", x);
+
+	return 0;
+}
+
+static int bme280_init_temperature(tmi_pressure_t *dev, uint8_t num)
+{
+
+	if (num >= BME280_GENERAL_OS_x16 && num < 0) {
+		printk("Invalid gyro fs range.");
+		return -ERANGE;
+	}
+	bme280_scale_general_t temperature_s = bme280_num_to_scale_sel(num);
+
+	int ret = write_mask(dev, BME280_REGISTER_CONTROL, 0xE0, (uint8_t)temperature_s);
+	if (ret != 0) {
+		return ret;
+	}
+	dev->config.temp = num;
+	return 0;
+}
+
+static int bme280_init_presssure(tmi_pressure_t *dev, uint8_t num)
+{
+	if (num >= BME280_GENERAL_OS_x16 && num < 0) {
+		printk("Invalid gyro fs range.");
+		return -ERANGE;
+	}
+	bme280_scale_general_t pressure_s = bme280_num_to_scale_sel(num);
+
+	int ret = write_mask(dev, BME280_REGISTER_CONTROL, 0x1C, (uint8_t)pressure_s);
+	if (ret != 0) {
+		return ret;
+	}
+
+	dev->config.press = num;
+	return 0;
+}
+
+static int bme280_get_temp(tmi_pressure_t *dev, float *temp_C)
+{
+	uint8_t tmp[3];
+	uint32_t combined_data;
+	read_reg(dev, BME280_REGISTER_TEMPDATA_H, tmp, sizeof(tmp));
+	// printk("0x%02X  0x%02X \n", tmp[0], tmp[1]);
+	uint16_t upper16 = sys_get_be16(&tmp[0]);
+	combined_data = (uint32_t)upper16 << 4;
+	combined_data |= (uint32_t)tmp[2] && 0xF0;
+	// printk("0x%04X \n", combined_data);
+	int32_t var1, var2;
+	var1 = ((combined_data / 8) - ((int32_t)dev->data.cal.dig_T1 * 2));
+	var1 = (var1 * ((int32_t)dev->data.cal.dig_T2)) / 2048;
+	var2 = ((combined_data / 16) - ((int32_t)dev->data.cal.dig_T1));
+	var2 = (((var2 * var2) / 4096) * ((int32_t)dev->data.cal.dig_T3)) / 16384;
+	dev->data.t_fine = var1 + var2 + dev->data.t_fine_adjust;
+	int32_t T = (dev->data.t_fine * 5 + 128) / 256;
+	// printk("check if t_fine is updated : %.3f \n", (float)dev->data.t_fine);
+	// printk("temperature from function : %.3f \n", (float)T / 100);
+	*temp_C = (float)T / 100;
+	return 0;
+}
+
+static int bme280_get_humidity(tmi_pressure_t *dev, float *hum_RH)
+{
+	uint8_t tmp[2];
+	read_reg(dev, BME280_REGISTER_HUMIDDATA_H, tmp, sizeof(tmp));
+	// printk("0x%02X  0x%02X \n", tmp[0], tmp[1]);
+	uint16_t raw = sys_get_be16(&tmp[0]);
+
+	int32_t var1, var2, var3, var4, var5;
+	float temp;
+	int ret = bme280_get_temp(dev, &temp);
+	if (ret != 0) {
+		return ret;
+	} // must be done first to get t_fine
+	var1 = dev->data.t_fine - ((int32_t)76800);
+	var2 = (int32_t)(raw * 16384);
+	var3 = (int32_t)(((int32_t)dev->data.cal.dig_H4) * 1048576);
+	var4 = ((int32_t)dev->data.cal.dig_H4) * var1;
+	var5 = (((var2 - var3) - var4) + (int32_t)16384) / 32768;
+	var2 = (var1 * ((int32_t)dev->data.cal.dig_H6)) / 1024;
+	var3 = (var1 * ((int32_t)dev->data.cal.dig_H3)) / 2048;
+	var4 = ((var2 * (var3 + (int32_t)32768)) / 1024) + (int32_t)2097152;
+	var2 = ((var4 * ((int32_t)dev->data.cal.dig_H2)) + 8192) / 16384;
+	var3 = var5 * var2;
+	var4 = ((var3 / 32768) * (var3 / 32768)) / 128;
+	var5 = var3 - ((var4 * ((int32_t)dev->data.cal.dig_H1)) / 16);
+	var5 = (var5 < 0 ? 0 : var5);
+	var5 = (var5 > 419430400 ? 419430400 : var5);
+	uint32_t H = (uint32_t)(var5 / 4096);
+
+	*hum_RH = (float)H / 1024.0;
+	return 0;
+}
+
+static int bme280_get_pressure(tmi_pressure_t *dev, float *press_Pa)
+{
+	uint8_t tmp[3];
+	uint32_t combined_data;
+	read_reg(dev, BME280_REGISTER_TEMPDATA_H, tmp, sizeof(tmp));
+	// printk("0x%02X  0x%02X \n", tmp[0], tmp[1]);
+	uint16_t upper16 = sys_get_be16(&tmp[0]);
+	combined_data = (uint32_t)upper16 << 4;
+	combined_data |= (uint32_t)tmp[2] && 0xF0;
+
+	int64_t var1, var2, var3, var4;
+	float temp;
+
+	int ret = bme280_get_temp(dev, &temp);
+	if (ret != 0) {
+		return ret;
+	}
+
+	var1 = ((int64_t)dev->data.t_fine) - 128000;
+	var2 = var1 * var1 * (int64_t)dev->data.cal.dig_P6;
+	var2 = var2 + ((var1 * (int64_t)dev->data.cal.dig_P5) * 131072);
+	var2 = var2 + (((int64_t)dev->data.cal.dig_P4) * 34359738368);
+	var1 = ((var1 * var1 * (int64_t)dev->data.cal.dig_P3) / 256) +
+	       ((var1 * ((int64_t)dev->data.cal.dig_P2) * 4096));
+	var3 = ((int64_t)1) * 140737488355328;
+	var1 = (var3 + var1) * ((int64_t)dev->data.cal.dig_P1) / 8589934592;
+
+	if (var1 == 0) {
+		return 0; // avoid exception caused by division by zero
+	}
+
+	var4 = 1048576 - combined_data;
+	var4 = (((var4 * 2147483648) - var2) * 3125) / var1;
+	var1 = (((int64_t)dev->data.cal.dig_P9) * (var4 / 8192) * (var4 / 8192)) / 33554432;
+	var2 = (((int64_t)dev->data.cal.dig_P8) * var4) / 524288;
+	var4 = ((var4 + var1 + var2) / 256) + (((int64_t)dev->data.cal.dig_P7) * 16);
+
+	*press_Pa = var4 / 256.0;
+	return 0;
+}
+
+static int bme280_activate_filter(tmi_pressure_t *dev, bme280_scale_filter_t scale_of_choice)
+{
+
+	if (scale_of_choice >= BME280_FILTER_OS_MAX && scale_of_choice < 0) {
+		printk("Invalid scale range.");
+		return -ERANGE;
+	}
+
+	int ret = write_mask(dev, BME280_REGISTER_CONFIG, 0x1C, (uint8_t)scale_of_choice);
+	if (ret != 0) {
+		return ret;
+	}
+
+	dev->config.filter = 2;
+	return 0;
+}
+
+static int bme280_init_all_default(tmi_pressure_t *dev)
+{
+	int ret = bme280_readCoefficients(dev);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+	ret = bme280_whoami(dev);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+	ret = bme280_init_temperature(dev, 2);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+	ret = bme280_init_humidity(dev, 2);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+	ret = bme280_init_presssure(dev, 2);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+
+	ret = bme280_activate_filter(dev, 2);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+
+	ret = bme280_activate_device(dev);
+	if (ret != 0) {
+		printk("Init failed: %d  \n", ret);
+	}
+	return 0;
+}
+
+const tmi_pressure_api_t bme280_api = {
+	.init = bme280_init_all_default,
+	.get_tempurature = bme280_get_temp,
+	.get_humidity = bme280_get_humidity,
+	.get_pressure = bme280_get_pressure,
+	.get_whoami = bme280_whoami,
+};
